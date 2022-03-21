3d2ef3c13beb8e5cad8524280d0c1eb252881079
Vansh Gupta <76569194+vanshgupta-2024@users.noreply.github.com>
2022-03-21 15:09:09 +0530

    Faq updated

--- assets/js/newSchedule.js
+++ assets/js/newSchedule.js
@@ -9,3 +9,16 @@ for(let i = 0; i < tabs.length; i++) {
         document.getElementById(this.getAttribute('data-content')).classList.add('schedule-content-selected');
     });
 }
+
+
+const tab = document.getElementsByClassName("schedule-tab-faq");
+for (let i = 0; i < tab.length; i++) {
+  tab[i].addEventListener("click", function () {
+    for (let j = 0; j < tab.length; j++) {
+      tab[j].classList.remove("selected-tab");
+      document.getElementById(tab[j].getAttribute("data-content")).classList.remove("schedule-content-selected");
+    }
+    this.classList.add("selected-tab");
+    document.getElementById(this.getAttribute("data-content")).classList.add("schedule-content-selected");
+  });
+}
